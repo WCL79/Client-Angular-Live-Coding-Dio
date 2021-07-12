fd16d8294d2c3ab8a64a1cb945ed0521025fb022
weslley lima <weslleyclima@yahoo.com.br>
2021-07-12 08:48:13 -0300

    Ajustar arquivo de configuração

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './environment.interface';
+
+export const environment: Environment = {
+  production: true,
+  URL_SERVIDOR: 'https://reuniao-de-sala-crud-spring.herokuapp.com',
 };
