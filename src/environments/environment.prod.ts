57b4d9f3c81c6b8e9c4231486b4e6bbb1b5f316d
weslley lima <weslleyclima@yahoo.com.br>
2021-07-12 11:19:08 -0300

    Ajustar caminho para api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  URL_SERVIDOR: 'hhttps://reuniao-de-sala-crud-spring.herokuapp.com',
+  URL_SERVIDOR: 'https://reuniao-de-sala-crud-spring.herokuapp.com/api/v1/salas',
 };
